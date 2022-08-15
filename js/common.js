62db1c5f4919c770220ec998497b11ba70ecb814
“nimoseel” <fromsomini@gmail.com>
2022-08-15 18:05:36 +0900

    Style: selected 기능 추가

--- js/common.js
+++ js/common.js
@@ -1,8 +1,16 @@
 const toggleBtn = document.querySelector('.nav-toggle');
 const nav = document.querySelector('.nav');
 const main = document.querySelector('.main');
+const navItem = document.querySelectorAll('.nav li a');
 
 toggleBtn.addEventListener('click', () => {
     nav.classList.toggle('active');
     main.classList.toggle('active');
 });
+
+function selectLink() {
+    navItem.forEach((item) => item.classList.remove('selected'));
+    this.classList.add('selected');
+}
+
+navItem.forEach((item) => item.addEventListener('click', selectLink));
