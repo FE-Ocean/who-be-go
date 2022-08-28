8d4170a7b848f8c2403e2db489f7f241da9fc0de
leehyeonseop <gustjq4146@naver.com>
2022-08-28 16:06:10 +0900

    Fix: 마이페이지 리뷰 더 불러오기

--- js/myPage.js
+++ js/myPage.js
@@ -8,33 +8,37 @@ const userIntro = document.querySelector('#text-intro');
 const reviewAlbum = document.querySelector('.review-album');
 const loading = document.querySelector('.wrapper-etc');
 hasToken();
-const createReviewAlbum = (review) => {
-    const li = document.createElement('li');
-    li.classList.add('card-s');
-    if (review.image) {
-        li.style.backgroundImage = `url(${review.image})`;
+const createReviewAlbum = (reviewList) => {
+    const fragment = document.createDocumentFragment();
+    for (let review of reviewList) {
+        const li = document.createElement('li');
+        li.classList.add('card-s');
+        if (review.image) {
+            li.style.backgroundImage = `url(${review.image})`;
+        }
+        else {
+            li.style.backgroundImage = `url(../assets/images/min_post_default.jpg)`;
+        }
+        const a = document.createElement('a');
+        a.href = `../pages/reviewDetail.html?id=${review.id}`;
+        const h3 = document.createElement('h3');
+        h3.textContent = '영화 제목';
+        h3.classList.add('visually-hidden');
+        const showContents = document.createElement('div');
+        showContents.classList.add('show-contents');
+        const movieTitle = document.createElement('strong');
+        movieTitle.id = 'movie-title';
+        movieTitle.textContent = review.content.split('@')[0];
+        const ratingStar = document.createElement('div');
+        const rating = parseFloat(review.content.split('@')[2]) * 20;
+        ratingStar.classList.add('rate-star');
+        ratingStar.style.setProperty('--width-rating', rating + '%');
+        showContents.append(movieTitle, ratingStar);
+        a.append(h3, showContents);
+        li.append(a);
+        fragment.appendChild(li);
     }
-    else {
-        li.style.backgroundImage = `url(../assets/images/min_post_default.jpg)`;
-    }
-    const a = document.createElement('a');
-    a.href = `../pages/reviewDetail.html?id=${review.id}`;
-    const h3 = document.createElement('h3');
-    h3.textContent = '영화 제목';
-    h3.classList.add('visually-hidden');
-    const showContents = document.createElement('div');
-    showContents.classList.add('show-contents');
-    const movieTitle = document.createElement('strong');
-    movieTitle.id = 'movie-title';
-    movieTitle.textContent = review.content.split('@')[0];
-    const ratingStar = document.createElement('div');
-    const rating = parseFloat(review.content.split('@')[2]) * 20;
-    ratingStar.classList.add('rate-star');
-    ratingStar.style.setProperty('--width-rating', rating + '%');
-    showContents.append(movieTitle, ratingStar);
-    a.append(h3, showContents);
-    li.append(a);
-    return li;
+    reviewAlbum.appendChild(fragment);
 };
 window.addEventListener('load', async () => {
     // 유저 정보 설정
@@ -45,8 +49,32 @@ window.addEventListener('load', async () => {
     userIntro.textContent = userInfo.intro;
     // 유저의 리뷰 앨범 설정
     const reviewList = await getReviewList();
-    for (let review of reviewList) {
-        reviewAlbum.appendChild(createReviewAlbum(review));
-    }
+    createReviewAlbum(reviewList);
     loading.classList.add('disabled');
+    createObserver(reviewAlbum.lastElementChild);
 });
+const createObserver = (element) => {
+    let skip = 10;
+    const observer = new IntersectionObserver(async ([entry], observer) => {
+        if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+            const newReviewList = await getReviewList(skip);
+            if (newReviewList.length === 0)
+                return;
+            createReviewAlbum(newReviewList);
+            if (reviewAlbum.lastElementChild instanceof HTMLLIElement) {
+                observer.observe(reviewAlbum.lastElementChild);
+            }
+            skip += 10;
+            if (newReviewList.length < 10) {
+                observer.disconnect();
+            }
+        }
+    }, {
+        threshold: 0.3,
+        root: reviewAlbum,
+    });
+    if (element) {
+        observer.observe(element);
+    }
+};
