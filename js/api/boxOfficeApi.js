dd70d3d1d58632984b6816b04feb49f7d946cdfb
leehyeonseop <gustjq4146@naver.com>
2022-09-01 11:38:38 +0900

    Fix: 1일 일때 날짜 0되는 오류 수정

--- js/api/boxOfficeApi.js
+++ js/api/boxOfficeApi.js
@@ -3,12 +3,13 @@ import { BOX_OFFICE_URL } from '../url/BASE_URL.js';
 const getBoxOfficeList = async () => {
     // 영화진흥위원회 서비스 키 값
     const key = '52ae81d6ce669361445e67ea47f30077';
-    // 날짜 만들어주기
-    const newDate = new Date();
-    const year = newDate.getFullYear();
-    const month = ('0' + (1 + newDate.getMonth())).slice(-2);
-    // 당일 자료는 나오지 않아서 하루 전날 값으로 대체했습니다.
-    const day = newDate.getDate() - 1;
+    // 현재 날짜
+    const now = new Date();
+    // 어제 날짜 당일 자료는 나오지 않아서 하루 전날 값으로 대체했습니다.
+    const yesterday = new Date(now.setDate(now.getDate() - 1));
+    const year = yesterday.getFullYear();
+    const month = ('0' + (1 + yesterday.getMonth())).slice(-2);
+    const day = yesterday.getDate();
     const today = year + month + day;
     const url = `${BOX_OFFICE_URL}key=${key}&targetDt=${today}`;
     const response = await fetch(url);
